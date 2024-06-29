bd5e63268541f94473b7502c8d5fd83bc1031d2f
--global <--global>
2024-06-29 11:02:32 -0500

    Finalize styles and script

--- Entrega5/script.js
+++ Entrega5/script.js
@@ -1,1 +1,24 @@
+const calculateArea = () => {
+  let base = document.getElementById("base").value;
+  let height = document.getElementById("height").value;
 
+  let area = base * height;
+
+  let perimeter = base * 2 + height * 2;
+
+  let diagonal = Math.sqrt(base ** 2 + height ** 2).toFixed(2);
+
+  let resultArea = (document.getElementById(
+    "result--area"
+  ).textContent = `El área es: ${area} cm²`);
+
+  let resultPerimeter = (document.getElementById(
+    "result--perimeter"
+  ).textContent = `El perímetro es: ${perimeter} cm`);
+
+  let resultDiagonal = (document.getElementById(
+    "result--diagonal"
+  ).textContent = `La diagonal es: ${diagonal} cm`);
+
+  return resultArea, resultPerimeter, resultDiagonal;
+};
